1a6d02a2d8811a5cd3d66811956cf051f54baca2
Ankur Seth <iankurseth@gmail.com>
2021-09-15 10:04:00 +0530

    listing args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+
+fn print_file_details(filepath: String) {
+    println!("Filepath: '{}'", filepath);
+}
+
 fn main() {
-    println!("Hello, world!");
+    if env::args().len() == 1 {
+        println!("Too few args!");
+        return;
+    }
+
+    for arg in env::args().skip(1) {
+        print_file_details(arg);
+    }
 }
